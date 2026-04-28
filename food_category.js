c1ca0ad99d89427a91c15e14318d5a7d2f7816ab
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-04-28 21:21:35 +0530

    Add files via upload

---
+++ food_category.js
@@ -0,0 +1,161 @@
+export const food = {
+  "Veg": [
+    {
+      name: "Paneer Butter Masala",
+      price: "₹250",
+      image: "https://cdn.tasteatlas.com/images/dishes/ceff65c140064d6285fc3814534a349b.jpg?w=1476&h=830",
+    },
+    {
+      name: "Veg Biryani",
+      price: "₹180",
+      image: "https://cookingfromheart.com/wp-content/uploads/2020/10/Avakaya-Biryani-4.jpg",
+    },
+    {
+      name: "Vegetarian Thali",
+      price: "₹210",
+      image: "https://eastindianrecipes.net/wp-content/uploads/2022/09/How-to-Make-North-Indian-Thali-Vegetarian-7-500x500.jpg",
+    },
+    {
+      name: "Pav Bhaji",
+      price: "₹110",
+      image: "https://assets.bonappetit.com/photos/63cb14735125107865c0fe8f/1:1/w_2560%2Cc_limit/012023-pav-bhaji-lede.jpg",
+    },
+    {
+      name: "Vegetable Pulao",
+      price: "₹149",
+      image: "https://www.whiskaffair.com/wp-content/uploads/2019/05/Veg-Pulao-1-3.jpg",
+    },   
+    {
+      name: "Mixed Vegetable Paratha",
+      price: "₹45",
+      image: "https://static.toiimg.com/thumb/53721534.cms?imgsize=315238&width=800&height=800",
+    },         
+    {
+      name: "Veg Noodle",
+      price: "₹120",
+      image: "https://www.indianhealthyrecipes.com/wp-content/uploads/2022/02/veg-noodles-vegetable-noodles-recipe.jpg",
+    }, 
+    {
+      name: "Dal Makhani",
+      price: "₹170",
+      image: "https://www.umami.recipes/api/image/recipes/Tc3VqCkMqD1ZhFgkNVw3/images/8N59Tyuz6qBUlgmL3HF0DX?w=3840&q=75",
+    },    
+  ],
+  "Non Veg": [
+    {
+      name: "Chicken Biryani",
+      price: "₹220",
+      image: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg",
+    },
+    {
+      name: "Non Veg Thali",
+      price: "₹199",
+      image: "https://www.champaranmeathousedaman.com/public/images/products/1732279849-non-veg-thali.webp",
+    },    {
+      name: "Chilli Paneer",
+      price: "₹110",
+      image: "https://www.indianhealthyrecipes.com/wp-content/uploads/2022/02/chilli-paneer-recipe.jpg",
+    },    {
+      name: "Mutton Curry",
+      price: "₹249",
+      image: "https://vismaifood.com/storage/app/uploads/public/86b/0bc/82c/thumb__700_0_0_0_auto.jpg",
+    },    {
+      name: "Tandoori Chicken kabab",
+      price: "₹220",
+      image: "https://simshomekitchen.com/wp-content/uploads/2022/04/tandoori-skewers.png",
+    },    
+    {
+      name: "Butter Chicken",
+      price: "₹260",
+      image: "https://images.immediate.co.uk/production/volatile/sites/30/2021/02/butter-chicken-ac2ff98.jpg?quality=90&resize=440,400",
+    },
+    {
+      name: "chilli chicken",
+      price: "₹220",
+      image: "https://theyummydelights.com/wp-content/uploads/2022/07/chilli-chicken-dry.jpg",
+    },   
+    {
+      name: "Mutton Korma",
+      price: "₹290",
+      image: "https://mughaldarbar.in/upload/pro/product-featured-74.jpg",
+    }   
+  ],
+  "South Indian": [
+    {
+      name: "Masala Dosa",
+      price: "₹120",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsa71IHGOIrZn61cbKemmBCu4vIBu8ws6NUg&s",
+    },
+    {
+      name: "South Indian Masala Vada",
+      price: "₹99",
+      image: "https://thumbs.dreamstime.com/b/vada-south-indian-food-isolated-transparent-background-369298162.jpg",
+    },    {
+      name: "Masala Idli",
+      price: "₹120",
+      image: "https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,q_auto,w_750,f_auto/f_auto/Idli-phplscEhG",
+    },    {
+      name: "Poha Tradition Lunch",
+      price: "₹150",
+      image: "https://www.holidify.com/images/cmsuploads/compressed/1024px-Sadhya_DSW_20200107182950.jpg",
+    },    {
+      name: "Paratha With Sambar",
+      price: "₹32",
+      image: "https://images.moneycontrol.com/static-mcnews/2021/04/paratha_shutterstock_1641709639.jpg?impolicy=website&width=1600&height=900",
+    },    
+    {
+      name: "Idli Sambar",
+      price: "₹80",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbzAj_KH0XbCrSdtGWb2Osghwnb45WnQe5WA&s",
+    },
+    {
+      name: "Dahi Vada",
+      price: "₹180",
+      image: "https://static.toiimg.com/thumb/55432577.cms?imgsize=240828&width=800&height=800",
+    },  
+    {  
+      name: "Thayir Vadai",
+      price: "₹260",
+      image: "https://images.slurrp.com/prod/recipe_images/transcribe/breakfast/Thayir-Vadai.webp",
+    }  
+  ],
+   "Chinese": [
+    {
+      name: "Hakka Noodles",
+      price: "₹150",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHI6vHr6V8yiVoYNezh3ViC4ZcCD7U_0kuuw&s",
+    },
+    {
+      name: "Spicy King Prawn",
+      price: "₹150",
+      image: "https://aluna.uk.com/wp-content/uploads/2024/06/3-spicy-Kkng-prawn.jpg",
+    },    {
+      name: "Veg Au Gratins",
+      price: "₹149",
+      image: "https://cdn.shopify.com/s/files/1/0565/2568/4873/files/Veg_Au_Gratin_Vegetable_Baked_Dish_480x480.jpg?v=1769151792",
+    },    {
+      name: "Chicken Continental",
+      price: "₹129",
+      image: "https://www.tasteofhome.com/wp-content/uploads/2017/09/Chicken-Continental-for-Four_exps43085_TH1789930B04_07_1bC_RMS.jpg",
+    },    {
+      name: "Creamy Garlic Mushroom Chicken",
+      price: "₹170",
+      image: "https://images.saymedia-content.com/.image/t_share/MTkyMTQ0ODE1NzQ2OTE3NzE0/10-easy-continental-dishes-you-can-cook-at-home.jpg",
+    },    
+    {
+      name: "Manchurian",
+      price: "₹170",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwlP7ovxUYJ_5aEbg65vQxy0AAaA3rO3xddg&s",
+    },
+    {
+      name: "Steamed Momos(Non-Veg)",
+      price: "₹130",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR9KvEnkyR7_CHbuFWOVW70qtwh_GLHV0OVA&s",
+    }, 
+    {
+      name: "Dim Sum",
+      price: "₹190",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSESKddZru7oYoNE2Gd_X9By6bKulhdMesSTQ&s",
+    },        
+  ]
+};
